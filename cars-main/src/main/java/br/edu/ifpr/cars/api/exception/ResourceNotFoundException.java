c370502f252dcc845b99dd9d120b4a1450c23788
Lucas <Lukinhasx248@gmail.com>
2025-10-21 15:21:42 -0300

    coloquei o código das exeções

---
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package main.java.br.edu.ifpr.cars.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+public ResourceNotFoundException(String msg) {
+super(msg);
+}
+}
